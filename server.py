741ba32e041aa12853cc402ac7e85d4cf5c0ec44
r.bukin <r.bukin@gmail.com>
2025-11-05 21:10:39 +0300

    fix: lock file

--- server.py
+++ server.py
@@ -56,42 +56,17 @@ app.type_clf = load_model('models/type.pkl')
 # app.lock = Lock()
 # app.lock = threading.Lock()
 
-def del_state(id):
-    state_file = os.path.join(FILE_PATH, f'{id}.pkl')
+def del_state(_id):
+    state_file = os.path.join(FILE_PATH, f'{_id}.pkl')
     if not os.path.exists(state_file):
         raise FileNotFoundError(f"Файл состояния не найден: {state_file}")
     
-    with open(state_file, "rb") as f:        
-        os.remove(state_file)
+    os.remove(state_file)
 
-
-# def set_state(id, state):
-#     with app.lock:
-#         with open(os.path.join(app.file_path, f'{id}.pkl'), 'wb') as f:
-#             pickle.dump(state, f)
-
-# def set_state(id, new_state):
-#     state_file = os.path.join(FILE_PATH, f'{id}.pkl')
-#     if not state_file.exists():
-#         raise FileNotFoundError(f"Файл состояния не найден: {state_file}")
-
-#     # Считаем текущее состояние из файла, если есть
-#     try:
-#         with open(state_file, 'rb') as f:
-#             current_state = pickle.load(f)
-#     except (FileNotFoundError, EOFError, pickle.UnpicklingError):
-#         current_state = {'page': 0, 'pages': 0}
-
-#     # Обновляем состояние только если новое впереди текущего
-#     if (new_state.get('page', 0) > current_state.get('page', 0) or
-#         new_state.get('pages', 0) > current_state.get('pages', 0)):
-#         with open(state_file, 'wb') as f:
-#             pickle.dump(new_state, f)
-
-def set_state(id: str, new_state: dict):
-    state_file = os.path.join(FILE_PATH, f'{id}.pkl')
-    lock_file = os.path.join(FILE_PATH, f'{id}.lock')
-    logger.debug(["set_state", id, new_state])
+def set_state(_id: str, new_state: dict):
+    state_file = os.path.join(FILE_PATH, f'{_id}.pkl')
+    lock_file = os.path.join(FILE_PATH, f'{_id}.lock')
+    logger.debug(["set_state", _id, new_state])
 
     with open(lock_file, "w") as lf:
         fcntl.flock(lf.fileno(), fcntl.LOCK_EX)  # эксклюзивная блокировка
@@ -115,8 +90,8 @@ def set_state(id: str, new_state: dict):
         finally:
             fcntl.flock(lf.fileno(), fcntl.LOCK_UN)  # снимаем блокировку
 
-def get_state(id):
-    state_file = os.path.join(FILE_PATH, f'{id}.pkl')
+def get_state(_id):
+    state_file = os.path.join(FILE_PATH, f'{_id}.pkl')
     if not os.path.exists(state_file):
         raise FileNotFoundError(f"Файл состояния не найден: {state_file}")
     with open(state_file, "rb") as f:
@@ -142,119 +117,77 @@ def get_file_name(fn, files):
     return fn
 
  
-def do_work(id):
+def do_work(_id):
     try:
         results = []
-        pdf_file_name = os.path.join(app.file_path, f'{id}.pdf')
-        zip_file_name = os.path.join(app.file_path, f'{id}')
+        pdf_file_name = os.path.join(app.file_path, f'{_id}.pdf')
+        zip_file_name = os.path.join(app.file_path, f'{_id}')
 
         splitter = PDFSplitter(zip_file_name, pdf_file_name,
                                app.orient_clf, app.type_clf, app.extractor)
 
-        state = get_state(id)
+        state = get_state(_id)
         for page, pages, info in splitter.process():            
             results.append(info)
             state = {'page': page, 'pages': pages}
             logger.debug(state)
-            set_state(id, state)
+            set_state(_id, state)
 
         state['results'] = results
-        state['url'] = f'result/{id}'
+        state['url'] = f'result/{_id}'
 
     except Exception as e:
         logger.exception(e)
         state = {'detail': str(e)}
     return state
 
-
-# def do_work(id):
-#     try:
-#         files = {}
-#         results = []
-#         pdf_file_name = os.path.join(app.file_path,f'{id}.pdf')
-#         zip_file_name = os.path.join(app.file_path,f'{id}.zip')
-#         with BytesIO() as archive:
-#             with ZipFile(archive, 'w') as zip_archive:
-
-#                 for page,pages,info,pdf in split_pdf_gen(pdf_file_name,app.orient_clf,app.type_clf,app.extractor):
-#                     file_name = get_file_name(info['sf_no'], files)
-#                     files[file_name] = file_name
-
-#                     with zip_archive.open(file_name+'.pdf', 'w') as pdf_file:
-#                         pdf_file.write(pdf.tobytes(garbage=4, deflate=True))
-
-
-#                     with zip_archive.open(file_name+'.json', 'w') as json_file:
-#                         json_file.write(bytes(json.dumps(info),'utf-8'))
-#                         # json.dump(info, json_file)
-#                     res = {'json':file_name+'.json', 'file': file_name+'.pdf', 'data': info, 'raw_data': info}
-#                     results.append(res)
-
-#                     state = {'page':page,'pages':pages}
-#                     set_state(id,state)
-
-#             with open(zip_file_name, 'wb') as f:
-#                 f.write(archive.getbuffer())
-#         state['results'] = results
-#         state['url'] = f'result/{id}.zip'
-
-#     except Exception as e:
-#         logger.exception(e)
-#         state = {'detail':str(e)}
-#     return state
-
-
 async def run_in_process(fn, *args):
     loop = asyncio.get_event_loop()
-    state = await loop.run_in_executor(app.state.executor, fn, *args)
-    logger.debug(["run_in_process", state])
-    set_state(id, state)
-
-
-async def cpu_bound_task(id: str) -> None:
-    await run_in_process(do_work, id)
+    await loop.run_in_executor(app.state.executor, fn, *args)
     
+
+
+async def cpu_bound_task(_id: str) -> None:
+    state = await run_in_process(do_work, _id)
+    set_state(_id, state)
 
 
 @app.post("/ocr", status_code=HTTPStatus.ACCEPTED)
 async def ocr(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
 
-    id = str(uuid4())
+    _id = str(uuid4())
 
-    set_state(id, {'page': 0, 'pages': 0})
+    set_state(_id, {'page': 0, 'pages': 0})
 
     # save input pdf file
-    out_file_name = f"output/{id}.pdf"
+    out_file_name = f"output/{_id}.pdf"
     async with aiofiles.open(out_file_name, 'wb') as out_file:
         content = await file.read()  # async read
         await out_file.write(content)  # async write
 
-    background_tasks.add_task(cpu_bound_task, id)
-    return JSONResponse({"id": id})
+    background_tasks.add_task(cpu_bound_task, _id)
+    return JSONResponse({"id": _id})
 
 
-@app.get('/progress/{id}')
-def status(id):
+@app.get('/progress/{_id}')
+def status(_id):
     try:
-        state = get_state(id)
+        state = get_state(_id)
         return state
     except Exception as e:
         logger.exception(e)
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.get('/result/{id}')
-def result(id):
+@app.get('/result/{_id}')
+def result(_id):
     try:
         #TODO state c ошибкой в нем нет page
-        state = get_state(id)
+        state = get_state(_id)
         if (state['page'] == 0 or state['page'] != state['pages']):
             raise HTTPException(status_code=404)
 
-        # del_state(id)
-        # os.remove(os.path.join(app.file_path, f'{id}.pdf'))
-
-        return FileResponse(path=os.path.join(app.file_path, f'{id}.zip'), media_type='application/zip')
+        return FileResponse(path=os.path.join(app.file_path, f'{_id}.zip'), media_type='application/zip')
     except Exception as e:
         logger.exception(e)
         raise HTTPException(status_code=500, detail=str(e))
